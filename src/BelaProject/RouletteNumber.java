e5d6b2a303b3c356d4808f4ee2dd5cc4adc9b463
Jeno Reich <reichjeno@gmail.com>
2019-05-21 18:22:54 +0100

    RouletteNumber class

---
+++ src/BelaProject/RouletteNumber.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BelaProject;
+
+/**
+ *
+ * @author Reich Jenő
+ */
+public class RouletteNumber {
+    private final int number;
+    private final Color color;
+
+    public RouletteNumber(int number, Color color) {
+        this.number = number;
+        this.color = color;
+    }
+
+    /**
+     * returns the number of the specific location of the roulette wheel
+     * @return 
+     */
+    public int getNumber() {
+        return number;
+    }
+
+    
+    /**
+     * returns the Color of the specific number
+     * @return 
+     */
+    public Color getColor() {
+        return color;
+    }
+    
+    
+    
+}
