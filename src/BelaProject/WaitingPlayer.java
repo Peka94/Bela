f8f4cb23324c121732149de34cdaa201cd4621b5
Attila <battila1995@gmail.com>
2019-05-23 09:45:53 +0200

    WaitingPlayer fix

--- src/BelaProject/WaitingPlayer.java
+++ src/BelaProject/WaitingPlayer.java
@@ -12,13 +12,10 @@ public class WaitingPlayer extends Player {
         super(startBudget, PlayerID.WAITING);
         waitingNumber = new Random().nextInt(7) + 4;
         expects = ProcessorBetOption.getRandom_ColorAs_BetOption();
-        setBet(new Random().nextInt(Integer.MAX_VALUE));
+        setBet(new Random().nextInt(Casino.MaxBet));
     }
 
-    public void whatGotRolled(BetOption color) throws Exception {
-        if (color != BetOption._BLACK || color != BetOption._RED) {
-            throw new Exception("Wrong enum");
-        }
+    public void whatGotRolled(BetOption color){
         if (color == expects) {
             expectSuccess++;
         } else {
